19ff87d503aff8b3e028c307483a6cd9e36b4c54
Tom Berry <tberry860@gmail.com>
2014-06-03 18:40:59 -0700

    pull extra info out of fields and pass into template as one dict

--- gargoyle/nexus_modules.py
+++ gargoyle/nexus_modules.py
@@ -9,6 +9,7 @@ gargoyle.nexus_modules
 import logging
 import nexus
 import os.path
+import json as real_json
 
 from functools import wraps
 
@@ -113,11 +114,18 @@ class GargoyleModule(nexus.NexusModule):
             return HttpResponseNotFound('Invalid sort order.')
 
         switches = list(Switch.objects.all().order_by(sort_by))
+        all_conditions = list(gargoyle.get_all_conditions())
+        extra_info = dict([
+            [cond.field.name, cond.field.extra_info]
+            for cond in all_conditions
+            if hasattr(cond.field, 'extra_info')
+        ])
 
         return self.render_to_response("gargoyle/index.html", {
             "switches": [s.to_dict(gargoyle) for s in switches],
-            "all_conditions": list(gargoyle.get_all_conditions()),
-            "sorted_by": sort_by
+            "all_conditions": all_conditions,
+            "sorted_by": sort_by,
+            "extra_info": real_json.dumps(extra_info),
         }, request)
 
     def add(self, request):
