19410e0efee4ff71d0c8c78b2e009911c7723df7
Tom Berry <tberry860@gmail.com>
2014-06-04 16:37:40 -0700

    fix call

--- gargoyle/media/js/events.js
+++ gargoyle/media/js/events.js
@@ -4,13 +4,13 @@ $(function() {
         $.getJSON('/account/accounts_for_email/{}/'.format(email), function(accounts) {
             $('#accountSelect').html(
                 _.map(
+                    accounts,
                     function(account) {
                         return '<option value="{}">{}</option>'.format(
                             account[0],
                             account[1]
                         );
-                    },
-                    accounts
+                    }
                 )
             );
         });
