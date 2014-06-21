2117090a9acb3806f8fa0666ab31eaad33e3c16d
Tom Berry <tberry860@gmail.com>
2014-06-20 17:48:49 -0700

    improve style

--- gargoyle/media/js/events.js
+++ gargoyle/media/js/events.js
@@ -1,22 +1,33 @@
+var insert_option_for_each_account = function(accounts, $input) {
+    var options = _.map(accounts,
+                        function(account) {
+                            return '<option value="{}">{}</option>'.format(account[0],
+                                                                           account[1]);
+                        });
+    $input
+        .parents('.conditionsForm')
+        .find('select.accountSelect')
+        .html(options.join(''));
+};
+
+
 $(function() {
-    $('table.switches').delegate('input.emailForAccount', 'blur', function() {
-        var that = this;
-        var email = encodeURIComponent($(this).val());
-        $.getJSON('/account/accounts_for_email/{}/'.format(email), function(accounts) {
-            $(that).parents('.conditionsForm').find('select.accountSelect').html(
-                _.map(
-                    accounts,
-                    function(account) {
-                        return '<option value="{}">{}</option>'.format(
-                            account[0],
-                            account[1]
-                        );
-                    }
-                ).join('')
-            );
-        });
-    }).delegate('button.findAccounts', 'click', function(e) {
-        e.preventDefault();
-        $('input.emailForAccount').blur();
-    });
+    $('table.switches')
+        .delegate('input.emailForAccount',
+                  'blur',
+                  function() {
+                      var that = this;
+                      var email = encodeURIComponent($(this).val());
+
+                      $.getJSON('/account/accounts_for_email/{}/'.format(email),
+                                function(accounts) {
+                                    insert_option_for_each_account(accounts, $(that));
+                                });
+                  })
+        .delegate('button.findAccounts',
+                  'click',
+                  function(e) {
+                      e.preventDefault();
+                      $('input.emailForAccount').blur();
+                  });
 });
